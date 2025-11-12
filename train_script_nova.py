69cc0f23fd9e3e1e805f28ddc697c12a48cb52fa
OrghoN <anoronyo@gmail.com>
2025-11-12 08:44:40 -0600

    Set config to variable
    The config file reference in the training script has been set to a
    variable and a config file for magMomentana has been screated

--- train_script_nova.py
+++ train_script_nova.py
@@ -2,7 +2,9 @@ import json
 
 from transformer_ee.train import MVtrainer
 
-with open("transformer_ee/config/input_nova_mprod6_1_OPAL_nd_fhc.json", encoding="UTF-8", mode="r") as f:
+config_path = "transformer_ee/config/input_nova_magMomentAna_nd_fhc.json"
+
+with open(config_path, encoding="UTF-8", mode="r") as f:
     input_d = json.load(f)
 
 # my_trainer = MVtrainer(input_d, logger=my_logger)
